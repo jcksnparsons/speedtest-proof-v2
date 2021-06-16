35852bdc7382621b09cbe24d6f8d2b3efe642d77
Jack Parsons <jackp@twinsunsolutions.com>
2021-06-16 15:15:05 -0500

    refactored to utilized speedtest gem, way easier than building own speedtest'

--- app/controllers/speedtest_controller.rb
+++ app/controllers/speedtest_controller.rb
@@ -2,6 +2,8 @@ class SpeedtestController < ApplicationController
   def index; end
 
   def perform
-    render json: { value: 'you performed a speed test' }
+    test = Speedtest::Test.new(upload_runs: 1)
+    results = test.run
+    render json: { value: results.upload_rate / 1024 / 1024 }
   end
 end
--- app/javascript/packs/speedtest.js
+++ app/javascript/packs/speedtest.js
@@ -1,18 +1,27 @@
-import Rails from '@rails/ujs'
+import Rails from "@rails/ujs";
+
+document.querySelector('.loader').style.display = 'none';
 
 export const speedTest = () => {
+  let fd = new FormData
+
+  document.querySelector('.loader').style.display = 'block';
+
   Rails.ajax({
     url: "/speedtest",
     type: "post",
+    contentType: "application/json",
     success: (data) => {
       console.log(data);
+      document.querySelector('.loader').style.display = 'none';
     },
     error: (data) => {
       console.log("you goofed");
+      document.querySelector('.loader').style.display = 'none';
     },
   });
 };
 
-document.getElementById('speedtest-perform').addEventListener('click', () => {
-    speedTest()
-})
+document.getElementById("speedtest-perform").addEventListener("click", () => {
+  speedTest();
+});
